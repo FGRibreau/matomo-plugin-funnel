82bbefd9a1dafe42de0307ca39469295244d62b9
François-Guillaume Ribreau <git@fgribreau.com>
2026-01-01 18:45:33 +0100

    fix: use configureDefinition() for Matomo 5.x option handling

--- Commands/ArchiveFunnel.php
+++ Commands/ArchiveFunnel.php
@@ -3,19 +3,33 @@
 namespace Piwik\Plugins\Funnels\Commands;
 
 use Piwik\Plugin\ConsoleCommand;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 use Piwik\Plugins\Funnels\API;
 use Piwik\Archive\Invalidator;
 use Piwik\Date;
 
 class ArchiveFunnel extends ConsoleCommand
 {
-    protected function configure()
+    protected function configure(): void
     {
-        $this->setName('funnels:rearchive')
-             ->setDescription('Invalidates reports for a specific funnel to trigger re-archiving.')
-             ->addOption('idsite', null, InputOption::VALUE_REQUIRED, 'ID Site')
-             ->addOption('idfunnel', null, InputOption::VALUE_REQUIRED, 'ID Funnel');
+        $this->setName('funnels:rearchive');
+        $this->setDescription('Invalidates reports for a specific funnel to trigger re-archiving.');
+    }
+
+    protected function configureDefinition(): void
+    {
+        $this->addOptionalValueOption(
+            'idsite',
+            null,
+            'ID Site'
+        );
+        $this->addOptionalValueOption(
+            'idfunnel',
+            null,
+            'ID Funnel'
+        );
     }
 
     protected function doExecute(): int
@@ -31,9 +45,7 @@ class ArchiveFunnel extends ConsoleCommand
             return self::FAILURE;
         }
 
-        // Validate Funnel exists
         try {
-            // We use View Access here as CLI is superuser usually, but let's check basic existence
             $funnel = API::getInstance()->getFunnel($idSite, $idFunnel);
             if (!$funnel) {
                 $output->writeln("<error>Funnel ID $idFunnel not found for Site $idSite.</error>");
@@ -46,29 +58,10 @@ class ArchiveFunnel extends ConsoleCommand
 
         $output->writeln("Invalidating reports for Funnel '$funnel[name]' (ID: $idFunnel)...");
 
-        // Invalidate
-        // Note: Matomo doesn't support invalidating *just* one plugin's blob easily via public API
-        // without invalidating the whole day/period for the site.
-        // `Invalidator` invalidates the Archive, which means ALL reports for that day/period.
-        // However, we can try to be specific if we knew the archive name, but Invalidator works on Site/Period level.
-
         $invalidator = new Invalidator();
-        // Invalidate for today and maybe past days?
-        // Usually when re-archiving a funnel, we want to re-process historical data.
-        // This is a heavy operation.
-        // Let's default to "Today" or allow a date range?
-        // For CLI "rearchive", users usually expect full history reprocessing or specific range.
-        // Let's invalidate "Today" by default to be safe, or explain usage.
-
-        // Actually, to re-process a funnel from scratch, we usually need to invalidate all periods.
-        // Let's just mark today for now as a safe default for testing.
-
         $invalidator->rememberToInvalidateArchivedReports($idSite, Date::today());
 
         $output->writeln("<info>Success. Reports for Site $idSite invalidated for today. Run core:archive to process.</info>");
-
-        // To truly re-archive historical data for a funnel, user should use core:invalidate-report-data
-        // This command is a helper alias mostly.
 
         return self::SUCCESS;
     }
